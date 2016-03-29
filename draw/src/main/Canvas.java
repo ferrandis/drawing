ca5259b44dc2e8f305450e6369e9d289c7d4d18a
Jungmin <jungminyoon4@gmail.com>
2016-03-29 10:13:51 +0100

    Realised that width of canvas has to be 2 more than the given width^

--- draw/src/main/Canvas.java
+++ draw/src/main/Canvas.java
@@ -1,38 +1,45 @@
+import java.awt.*;
 import java.io.PrintWriter;
 
 public class Canvas {
+    private final PrintWriter writer;
     private final int width;
     private final int height;
 
-    public Canvas(int width, int height) {
+    public Canvas(PrintWriter writer, int width, int height) {
+        this.writer = writer;
 
         this.width = width;
         this.height = height;
     }
 
-    public void draw(PrintWriter printWriter) {
-
-        drawHorizontalLine(printWriter);
+    public void drawCanvas() {
+        drawHorizontalLine(writer);
         for (int i = 0; i < height; i++) {
-            drawVerticalLine(printWriter);
+            drawVerticalLine(writer);
         }
-        drawHorizontalLine(printWriter);
+        drawHorizontalLine(writer);
 
-        printWriter.flush();
+        writer.flush();
     }
 
     private void drawHorizontalLine(PrintWriter printWriter){
-        for (int i = 0; i < width; i++) {
+        for (int i = 0; i < width+2; i++) {
             printWriter.print("-");
         }
         printWriter.print("\n");
     }
+
     private void drawVerticalLine(PrintWriter printWriter){
         printWriter.print("|");
-        for (int i = 0; i < width-2; i++) {
+        for (int i = 0; i < width; i++) {
             printWriter.print(" ");
 
         }
         printWriter.print("|\n");
+    }
+
+    public void drawLine(Point start, Point end) {
+
     }
 }
--- draw/test/CanvasTest.java
+++ draw/test/CanvasTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import java.awt.*;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
 
@@ -7,18 +8,36 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class CanvasTest {
+
     @Test
     public void canDrawACanvas(){
-        Canvas canvas = new Canvas(5, 2);
-
-        String expectedCanvas = "-----\n"
-                + "|   |\n"
-                + "|   |\n"
-                + "-----\n";
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        canvas.draw(new PrintWriter(outputStream));
+        Canvas canvas = new Canvas(new PrintWriter(outputStream), 5, 2);
+
+        canvas.drawCanvas();
+        String expectedCanvas = "-------\n"
+                + "|     |\n"
+                + "|     |\n"
+                + "-------\n";
+
         assertThat(outputStream.toString(), equalTo(expectedCanvas));
+
+    }
+
+    @Test
+    public void canDrawAHorizontalLine(){
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        Canvas canvas = new Canvas(new PrintWriter(outputStream), 20, 4);
+
+        canvas.drawLine(new Point(1,2), new Point(6,2));
+        String expectedCanvas = "----------------------\n"
+                + "|                    |\n"
+                + "|xxxxxx              |\n"
+                + "|                    |\n"
+                + "|                    |\n"
+                + "----------------------\n";
 
     }
 }
